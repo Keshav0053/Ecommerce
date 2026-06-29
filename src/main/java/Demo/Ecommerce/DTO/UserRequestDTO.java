5819295169b4b87f1fc2aa78192df7149e4536b6
keshav <kumarkeshav7035@gmail.com>
2026-06-30 04:10:11 +0530

    update user

---
+++ src/main/java/Demo/Ecommerce/DTO/UserRequestDTO.java
@@ -0,0 +1,15 @@
+package Demo.Ecommerce.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserCreateRequestDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String mobile;
+    private String password;
+    private String role;
+}
